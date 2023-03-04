69b3457de710ee868964904fbdbb318d48f46647
Rinnegatamante <rinnegatamante@gmail.com>
2023-03-04 19:39:23 +0100

    Bind Start, L and R to pause, goals, fast forward.

--- loader/main.c
+++ loader/main.c
@@ -1306,6 +1306,9 @@ int ctrl_thread(SceSize args, void *argp) {
 
 	int lastX[2] = { -1, -1 };
 	int lastY[2] = { -1, -1 };
+	int lastStart = 0;
+	int lastRT = 0;
+	int lastLT = 0;
 
 	while (1) {
 		SceTouchData touch;
@@ -1329,6 +1332,33 @@ int ctrl_thread(SceSize args, void *argp) {
 				lastY[i] = -1;
 			}
 		}
+		
+		SceCtrlData pad;
+		sceCtrlPeekBufferPositive(0, &pad, 1);
+		int currStart = (pad.buttons & SCE_CTRL_START) ? 1 : 0;
+		int currLT = (pad.buttons & SCE_CTRL_LTRIGGER) ? 1 : 0;
+		int currRT = (pad.buttons & SCE_CTRL_RTRIGGER) ? 1 : 0;
+		if (currStart != lastStart) {
+			if (!lastStart)
+				Java_com_android_Game11Bits_GameLib_touchDown(fake_env, NULL, touch.reportNum, 920, 30);
+			else
+				Java_com_android_Game11Bits_GameLib_touchUp(fake_env, NULL, touch.reportNum, 920, 30);
+		}
+		if (currLT != lastLT) {
+			if (!lastLT)
+				Java_com_android_Game11Bits_GameLib_touchDown(fake_env, NULL, touch.reportNum, 31, 31);
+			else
+				Java_com_android_Game11Bits_GameLib_touchUp(fake_env, NULL, touch.reportNum, 31, 31);
+		}
+		if (currRT != lastRT) {
+			if (!lastRT)
+				Java_com_android_Game11Bits_GameLib_touchDown(fake_env, NULL, touch.reportNum, 925, 505);
+			else
+				Java_com_android_Game11Bits_GameLib_touchUp(fake_env, NULL, touch.reportNum, 925, 505);
+		}
+		lastRT = currRT;
+		lastLT = currLT;
+		lastStart = currStart;
 
 		sceKernelDelayThread(1000);
 	}
@@ -1348,8 +1378,6 @@ int main(int argc, char *argv[]) {
 	scePowerSetBusClockFrequency(222);
 	scePowerSetGpuClockFrequency(222);
 	scePowerSetGpuXbarClockFrequency(166);
-
-	pstv_mode = sceCtrlIsMultiControllerSupported() ? 1 : 0;
 
 	if (check_kubridge() < 0)
 		fatal_error("Error kubridge.skprx is not installed.");
